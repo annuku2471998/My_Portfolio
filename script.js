f15781d3f23b2aba409fd7fdb4a46d0902c1eb6a
annuku2471998 <annuku2471998@gmail.com>
2026-02-24 02:56:13 +0530

    add script and humbarger menu

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
 function toggleMenu() {
-     document.getElementById('nav-links').classList.toggle('active'); 
-      }
+    const navLinks = document.getElementById("nav-links");
+    
+    // Agar mobile screen hai tabhi 'active' class toggle hogi
+    if (window.innerWidth <= 720) {
+        navLinks.classList.toggle("active");
+    }
+}
